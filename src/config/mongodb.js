51809a722013a271fcc704a43fbc27cf39b73a32
phung loc <phungloc6102003@gmail.com>
2025-01-10 20:28:00 +0700

    success connect_mongoDB

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,38 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { env } from "~/config/environment";
+
+//Khởi tạo một đối tượng trelloDatabaseInstance ban đầu là null (vì chúng ta chưa connect)
+let trelloDatabaseInstance = null;
+// Khởi tạo một đối tượng mongoClientInstance  để kết nối tới MongoDB
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  //Lưu ýý:Cái serverApi có phiên bản MongoDB 5.0.0 trở lên,có thể không cần dùng nó,còn nêú dùng nó là chúng ta chỉ định một cái Stable API version của MongoDB
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const CONNECT_DB = async () => {
+  //gội kết nối với mongoDB atlas với URI đã khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect();
+  //Kết nối thành công thì lấy ra DB theo tên và gán ngược nó lại vào biến trelloDatabaseInstance ở trên của chúng ta
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME);
+};
+
+//Đóng kết nối tới Database khi cần
+export const CLOSE_DB = async () => {
+  console.log("code chay vao cho close nay");
+  await mongoClientInstance.close();
+};
+
+//function GET_DB (không async) này có nhiệm vụ export ra cái trello Database sau khi đã connect thành công tới mongoDB để chúng ta sử dụng ở nhiều nơi khác nhau trong code.
+//Lưu ý đảm bảo chỉ luôn gọi cái getDB này sau khi đã kết nối thành công tới MongoDB
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error("Must connect to db first!");
+  return trelloDatabaseInstance;
+};
--- src/server.js
+++ src/server.js
@@ -4,29 +4,50 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 
-import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import express from "express";
+import { CONNECT_DB, GET_DB, CLOSE_DB } from "~/config/mongodb";
+import exitHook from "async-exit-hook";
+import { env } from "~/config/environment";
+const START_SERVER = () => {
+  const app = express();
 
-const app = express()
+  app.get("/", async (req, res) => {
+    res.end("<h1>Hello World!</h1><hr>");
+  });
 
-const hostname = 'localhost'
-const port = 8017
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(
+      `3.Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`
+    );
+  });
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  //thực hiện các tác vụ cleanup trước khi đóng server
+  exitHook(() => {
+    console.log("4.Disconnecting from MongoDB Cloud Atlas...");
+    CLOSE_DB();
+    console.log("5.Disconnected from MongoDB Cloud Atlas");
+  });
+};
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+//IIFE ->Immediately Invoked Function Expression  ->là một hàm ẩn danh -> chạy ngay sau khi được định nghĩa
+(async () => {
+  try {
+    console.log("1.Connecting to MongoDB Cloud Atlas");
+    await CONNECT_DB();
+    console.log("2.Connected to MongoDB Cloud Atlas");
+    START_SERVER();
+  } catch (error) {
+    console.error(error);
+    process.exit(0);
+  }
+})();
+
+//Chỉ khi kết nối tới DB thành công thì mới Start Server backend lên.
+// CONNECT_DB()
+//   .then(() => console.log("Connected to MongoDB Cloud Atlas"))
+//   .then(() => START_SERVER())
+//   .catch((err) => {
+//     console.error(err);
+//     process.exit(0);
+//   });
