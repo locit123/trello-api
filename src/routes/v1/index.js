33088d18e8a5d89a7b346cf85c2f29900c9f1776
phung loc <phungloc6102003@gmail.com>
2025-01-10 22:49:33 +0700

    code express-router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,3 +3,16 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+import { boardRouters } from "~/routes/v1/boardRouters";
+const Router = express.Router();
+//check APIs v1/status
+Router.get("/status", (req, res) => {
+  res.status(StatusCodes.OK).json({ message: "Api V1 are ready to use" });
+});
+//Board APIs
+Router.use("/board", boardRouters);
+
+export const APIs_V1 = Router;
